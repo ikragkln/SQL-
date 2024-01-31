06d0d2e1f130e6b466350c54cb677228f08ab0d2
ikragkln <104828238+ikragkln@users.noreply.github.com>
2024-02-01 00:43:34 +0300

    Add files via upload

---
+++ SQL başlangıç.sql
@@ -0,0 +1,7 @@
+--SELECT column1 FROM table1,ilk başa istenen sütun başlığı yazılır nereden alınacağı ise FROM dan sonra belirtilir.
+--sql komutları case sensitive değildir yani büyük küçük harfle de çalısır
+-- ; birden fazla sorguyu ayırmamızı sağlar
+--SELECT: veri almak veri sıralamak için kullanılır
+--SQL deklaratif yani beyan edici dildir.Önemli olan sonucları almak
+--SELECT title,description FROM film;
+SELECT first_name,last_name FROM actor;
